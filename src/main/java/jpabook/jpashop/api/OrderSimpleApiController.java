27ac0a61c745f427a493285611f1e30b9b9224dc
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-08-02 03:12:01 +0900

    주문 조회 v1

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.Domain.Order;
+import jpabook.jpashop.Repository.OrderRepository;
+import jpabook.jpashop.Repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for(Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+
+        return all;
+    }
+}
